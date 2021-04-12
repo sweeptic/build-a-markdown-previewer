27c61ec5fc901549ec1f34be6b5ae060d902a2bb
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-12 10:15:13 +0200

    basic setup for application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Editor from './Editor';
+import Previewer from './Previewer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App container border border-dark mx-auto p-5 m-5'>
+      <h1 className='m-5'>Markdown Previewer</h1>
+      <Editor />
+      <Previewer />
     </div>
   );
 }
---
+++ src/Previewer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Previewer = () => {
+  return <div>previewer</div>;
+};
+
+export default Previewer;
